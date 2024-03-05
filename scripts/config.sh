7990c54b2be33b34c602807ad59f0a6f602915d1
alexandrelutt <alexandre.lutt@eleves.enpc.fr>
2024-03-05 11:56:18 +0100

    fix loss

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,7 +3,7 @@ pip install -r requirements.txt
 
 sh scripts/build_extensions.sh
 
-python3 -m pip uninstall torch torchvision
+python3 -m pip uninstall -y torch torchvision
 python3 -m pip freeze | grep nvidia- | xargs pip uninstall -y
 python3 -m pip install torch torchvision
 

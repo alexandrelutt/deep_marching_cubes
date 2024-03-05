65f8e1b8288e2600dcff0aadee7ff76b780f6d96
alexandrelutt <alexandre.lutt@eleves.enpc.fr>
2024-03-05 11:27:46 +0100

    fix config

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,3 +3,9 @@ source venv/bin/activate
 python3 -m pip install --upgrade pip
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements.txt
+
+sh scripts/build_extensions.sh
+
+python3 -m pip uninstall torch torchvision
+python3 -m pip freeze | grep nvidia- | xargs pip uninstall -y
+python3 -m pip install torch torchvision

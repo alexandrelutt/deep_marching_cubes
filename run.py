bce008336cbe778f67533f3cf1cc1ff42e981f62
alexandrelutt <alexandre.lutt@eleves.enpc.fr>
2024-03-05 15:16:06 +0100

    only to add more losses

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     batch_size = 8
-    n_epochs = 10
+    n_epochs = 10 #500
     learning_rate = 5e-4
 
     train_loader = get_loader(set='train', batch_size=batch_size)

4eaaa63fdba14f7097e3edce00df31f2f46ebf9c
alexandrelutt <alexandre.lutt@eleves.enpc.fr>
2024-03-05 15:17:49 +0100

    adding weight_decay

--- run.py
+++ run.py
@@ -14,13 +14,14 @@ if __name__ == '__main__':
     batch_size = 8
     n_epochs = 10 #500
     learning_rate = 5e-4
+    weight_decay = 1e-3
 
     train_loader = get_loader(set='train', batch_size=batch_size)
     test_loader = get_loader(set='test', batch_size=batch_size)
 
     model = DeepMarchingCube()
     loss_module = MyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
     train_losses, test_losses, best_model = train(model, train_loader, test_loader, loss_module, n_epochs, optimizer, device)
 

a4361c87db94d5c29953c048ac7ac598f7351709
alexandrelutt <alexandre.lutt@eleves.enpc.fr>
2024-03-06 16:25:23 +0100

    fix batch size

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    batch_size = 8
+    batch_size = 4
     n_epochs = 10 #500
     learning_rate = 5e-4
     weight_decay = 1e-3

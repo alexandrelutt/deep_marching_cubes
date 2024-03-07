0cad7fce55728c345ca37134c7ad8ad96a8b648c
alexandrelutt <alexandre.lutt@eleves.enpc.fr>
2024-03-08 00:22:34 +0100

    fix order

--- scripts/build_extensions.sh
+++ scripts/build_extensions.sh
@@ -1,9 +1,3 @@
-python scripts/build_extension_curvature.py install
-# cd dist
-# unzip curvature_extension-0.0.0-py3.10-linux-x86_64.egg
-# cd ..
-# mv dist curvature_extension
-
 python scripts/build_extension_occto.py install
 cd dist
 unzip occtopology_extension-0.0.0-py3.10-linux-x86_64.egg
@@ -15,3 +9,9 @@ cd dist
 unzip distance_extension-0.0.0-py3.10-linux-x86_64.egg
 cd ..
 mv dist distance_extension
+
+python scripts/build_extension_curvature.py install
+# cd dist
+# unzip curvature_extension-0.0.0-py3.10-linux-x86_64.egg
+# cd ..
+# mv dist curvature_extension

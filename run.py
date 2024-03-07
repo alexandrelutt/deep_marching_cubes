970a393229d097e613a736f5119ac6a11768d535
alexandrelutt <alexandre.lutt@eleves.enpc.fr>
2024-03-08 00:51:06 +0100

    clean run

--- run.py
+++ run.py
@@ -1,39 +1,32 @@
 import torch
 import faulthandler
 
-# from code.model import DeepMarchingCube
+from code.model import DeepMarchingCube
 from code.loss import MyLoss
-# from code.loader import get_loader
-# from code.train import train
+from code.loader import get_loader
+from code.train import train
 
 if __name__ == '__main__':
     faulthandler.enable()
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # batch_size = 8
-    # n_epochs = 10 #500
-    # learning_rate = 5e-4
-    # weight_decay = 1e-3
+    batch_size = 8
+    n_epochs = 10 #500
+    learning_rate = 5e-4
+    weight_decay = 1e-3
 
-    # train_loader = get_loader(set='train', batch_size=batch_size)
-    # test_loader = get_loader(set='test', batch_size=batch_size)
+    train_loader = get_loader(set='train', batch_size=batch_size)
+    test_loader = get_loader(set='test', batch_size=batch_size)
 
-    # model = DeepMarchingCube()
+    model = DeepMarchingCube()
     loss_module = MyLoss()
-    # optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
-    # train_losses, test_losses, best_model = train(model, train_loader, test_loader, loss_module, n_epochs, optimizer, device)
+    train_losses, test_losses, best_model = train(model, train_loader, test_loader, loss_module, n_epochs, optimizer, device)
 
-    # print('Train losses:')
-    # print(train_losses)
+    print('Train losses:')
+    print(train_losses)
 
-    # print('Test losses:')
-    # print(test_losses)
-
-    offset = torch.rand(8, 3, 33, 33, 33)
-    topology = torch.rand(8, 3, 33, 33, 33)
-    clean_batch = torch.rand(8, 3, 33, 33, 33)
-    occupancy = torch.rand(8, 1, 33, 33, 33)
-
-    loss = loss_module.loss(offset, topology, clean_batch, occupancy)
+    print('Test losses:')
+    print(test_losses)
--- scripts/main_setup.sh
+++ scripts/main_setup.sh
@@ -1,7 +1,7 @@
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements.txt
 
-sh scripts/build_extensions.sh
+# sh scripts/build_extensions.sh
 
 # python3 -m pip uninstall -y torch torchvision
 # python3 -m pip freeze | grep nvidia- | xargs pip uninstall -y

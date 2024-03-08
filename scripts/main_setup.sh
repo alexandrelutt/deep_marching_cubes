05da02ed739ab7cbf1d7a10f8131f4eb71454957
alexandrelutt <alexandre.lutt@eleves.enpc.fr>
2024-03-08 01:05:46 +0100

    unlock

--- scripts/main_setup.sh
+++ scripts/main_setup.sh
@@ -1,15 +1,15 @@
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements.txt
 
-# sh scripts/build_extensions.sh
+sh scripts/build_extensions.sh
 
-# python3 -m pip uninstall -y torch torchvision
-# python3 -m pip freeze | grep nvidia- | xargs pip uninstall -y
-# python3 -m pip install torch torchvision
+python3 -m pip uninstall -y torch torchvision
+python3 -m pip freeze | grep nvidia- | xargs pip uninstall -y
+python3 -m pip install torch torchvision
 
-# sh scripts/download_data.sh
-# sh scripts/fix_cudnn.sh
+sh scripts/download_data.sh
+sh scripts/fix_cudnn.sh
 
-# rm -r occtopology_extension.egg-info
-# rm -r distance_extension.egg-info
-# rm -r curvature_extension.egg-info
+rm -r occtopology_extension.egg-info
+rm -r distance_extension.egg-info
+rm -r curvature_extension.egg-info

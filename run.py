eac1a868a463827077c2037efb0c57f608b89d77
alexandrelutt <alexandre.lutt@eleves.enpc.fr>
2024-03-10 17:29:27 +0100

    no paralel

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import torch
-import torch.nn as nn
 import faulthandler
 
 from code.model import DeepMarchingCube
@@ -13,7 +12,7 @@ if __name__ == '__main__':
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    batch_size = 8*4
+    batch_size = 8
     n_epochs = 50 # base 500
     learning_rate = 5e-4 ## base 5e-4
     weight_decay = 1e-3
@@ -22,7 +21,6 @@ if __name__ == '__main__':
     test_loader = get_loader(set='test', batch_size=batch_size)
 
     model = DeepMarchingCube()
-    model= nn.DataParallel(model)
     loss_module = MyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=2)

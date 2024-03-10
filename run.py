8c14924d67d09bffab3f0a3845cf90692cb9f055
alexandrelutt <alexandre.lutt@eleves.enpc.fr>
2024-03-10 17:06:17 +0100

    add parallel

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import faulthandler
 
 from code.model import DeepMarchingCube
@@ -14,13 +15,14 @@ if __name__ == '__main__':
 
     batch_size = 8
     n_epochs = 50 # base 500
-    learning_rate = 5e-3 ## base 5e-4
+    learning_rate = 5e-4 ## base 5e-4
     weight_decay = 1e-3
 
     train_loader = get_loader(set='train', batch_size=batch_size)
     test_loader = get_loader(set='test', batch_size=batch_size)
 
     model = DeepMarchingCube()
+    model= nn.DataParallel(model)
     loss_module = MyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=2)

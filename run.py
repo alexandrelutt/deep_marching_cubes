8a20ecf04f09834efc0b5c7f26b2e4229445d053
alexandrelutt <alexandre.lutt@eleves.enpc.fr>
2024-03-10 15:20:59 +0100

    bigger lr

--- run.py
+++ run.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    batch_size = 32 # base 8
+    batch_size = 8
     n_epochs = 50 # base 500
-    learning_rate = 5e-4 ## base 5e-4
+    learning_rate = 5e-3 ## base 5e-4
     weight_decay = 1e-3
 
     train_loader = get_loader(set='train', batch_size=batch_size)

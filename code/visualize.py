f88fae99c6eed87b9a458e4a5670ab3c52654a4a
alexandrelutt <alexandre.lutt@eleves.enpc.fr>
2024-03-18 23:44:23 +0100

    print

--- code/visualize.py
+++ code/visualize.py
@@ -162,7 +162,8 @@ def get_chamfer_dist(true_points, occupancy, grid):
     dist = 0
     for i in range(batch_size):
         pred_points = np.array([xv_cls, yv_cls, zv_cls]).T
-        # pred_points = pred_points[occupancy[i].flatten() > proba_treshold]
+        pred_points = pred_points[occupancy[i].flatten() > proba_treshold]
+        print(pred_points)
 
         dists_pc1_to_pc2 = np.sqrt(((pred_points[:, np.newaxis] - true_points[i]) ** 2).sum(axis=-1).min(axis=-1))
         dists_pc2_to_pc1 = np.sqrt(((true_points[i][:, np.newaxis] - pred_points) ** 2).sum(axis=-1).min(axis=-1))
